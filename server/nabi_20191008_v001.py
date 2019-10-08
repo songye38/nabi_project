a2af003ef9df06956ca0f469e43a96c000f44702
박송이 <vsongyev@hanmail.net>
2019-10-08 07:58:25 +0000

    return 부분에 message 추가

--- server/nabi_20191008_v001.py
+++ server/nabi_20191008_v001.py
@@ -18,7 +18,7 @@ def register():
     curs.execute(search_query)
     rows = curs.fetchall()
     if len(rows) >0:
-        return { "result": "fail", "value": data}
+        return { "result": "fail", "value":data,"message":"이미 계정이 존재합니다."}
     else:
         data["pwd"] = hashlib.sha256(data["pwd"].encode()).hexdigest()
         sql = "INSERT INTO user(user_email,user_pwd,user_name) VALUES(" +'"'+data["email"] + '"'+","+'"'+data["pwd"]+'"' +","+'"' +data["name"]+'"'+")"
@@ -29,21 +29,20 @@ def register():
             print(e)
         finally:
             curs.close()
-            print("save success!")
-            return { "result": "success", "value": data}
+            return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 #===============================================================================================로그인
  #로그인 관련 API part 
 @app.route('/login',methods=['POST'])
 def login():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"로그인하였습니다."}
     
 #===============================================================================================비밀번호 찾기 기능
 @app.route('/findPwd',methods=['POST'])
 def findPwd():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
     # if _mode =='input':
     #     data = request.json
     #     return { "result": "success", "value": data}
@@ -58,7 +57,7 @@ def findPwd():
 @app.route('/main/<_userId>',methods=['GET'])
 def main(_userId):
     result = {"userId": _userId}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 
 #===============================================================================================검색탭
@@ -66,12 +65,12 @@ def main(_userId):
 @app.route('/comSearch/<_keyword>',methods=['GET'])
 def comShow(_keyword):
     result = {"keyworkd": _keyword}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
     
 @app.route('/comSearch',methods=['POST'])
 def comSearch():
         data = request.json
-        return { "result": "success", "value": data}
+        return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 #===============================================================================================개별 비대위 페이지
 #개별 비대위 페이지 API part
@@ -79,142 +78,142 @@ def comSearch():
 @app.route('/<_comNo>/onGoing/<_order>',methods=['GET'])
 def comOnGoing(_comNo,_order):
     result = {"committeeNo": _comNo, "order ": _order}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 #내가 참여했던 행동들, 대화 보여주기 -> 내가 과거에 참여한 것들의 목록만 알기 위해서 나의 id가 필요함
 @app.route('/<_comNo>/participate/<_userId>/<_order>',methods=['GET'])
 def comParticipate(_comNo,_order,_userId):
     result = {"committeeNo": _comNo, "order": _order}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 #내가 참여하진 않았지만 비대위에서 과거에 진행했었던 것들 목록 보여주기 
 @app.route('/<_comNo>/archive/<_order>',methods=['GET'])
 def comArchive(_comNo,_order):
     result = {"committeeNo": _comNo, "order": _order}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 #확인할 수 있는 정보 목록 보여주기 
 @app.route('/<_comNo>/info/<_order>',methods=['GET'])
 def comInfo(_comNo,_order):
     result = {"committeeNo": _comNo, "order": _order}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 
 #===============================================================================================온라인 행동 관련
 
 @app.route('/on/listup/<_comNo>',methods=['GET'])
 def onListup(_comNo):
-    return { "result": "success", "value": "return online action list"}
+    return { "result": "success", "value": "return online action list","message":"성공적으로 조회하였습니다."}
 
 @app.route('/on/read/<_comNo>/<_id>',methods=['GET'])
 def onRead(_comNo,_id):
     result = {"committeeNo": _comNo, "articleNo": _id}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 #===============================================================================================오프라인 행동 관련
 
 @app.route('/off/listup/<_comNo>',methods=['GET'])
 def offListup(_comNo):
     result = {"committeeNo": _comNo}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 @app.route('/off/read/<_comNo>/<_id>',methods=['GET'])
 def offRead(_comNo,_id):
     result = {"committeeNo": _comNo, "articleNo": _id}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 @app.route('/off/write',methods=['POST'])
 def offWrite():
         data = request.json
-        return { "result": "success", "value": data}
+        return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 @app.route('/off/delete',methods=['POST'])
 def offDelete():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 #===============================================================================================대화하기 관련
 
 @app.route('/talk/listup/<_comNo>',methods=['GET'])
 def talkListup(_comNo):
     result = {"committeeNo": _comNo}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 @app.route('/talk/read/<_comNo>/<_id>',methods=['GET'])
 def talkRead(_comNo,_id):
     result = {"committeeNo": _comNo,"articleId":_id}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 @app.route('/talk/write',methods=['POST'])
 def talkWrite():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 @app.route('/talk/update',methods=['POST'])
 def talkUpdate():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 @app.route('/talk/delete',methods=['POST'])
 def talkDelete():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 
 #===============================================================================================자료공유 관련
 @app.route('/share/listup/<_comNo>',methods=['GET'])
 def shareListup(_comNo):
     result = {"committeeNo": _comNo}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 @app.route('/share/read/<_comNo>/<_id>',methods=['GET'])
 def shareRead(_comNo,_id):
     result = {"committeeNo": _comNo,"articleId":_id}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 @app.route('/share/write',methods=['POST'])
 def shareWrite():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 @app.route('/share/update',methods=['POST'])
 def shareUpdate():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 @app.route('/share/delete',methods=['POST'])
 def shareDelete():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 #===============================================================================================포인트 관련
 @app.route('/point/<_userId>/<_mode>',methods=['GET'])
 def checkPoint(_userId,_mode):
     if _mode == 'rank':
-        return { "result": "success", "value": '나의 순위를 볼 수 있는 페이지 렌더링'}
+        return { "result": "success", "value": '나의 순위를 볼 수 있는 페이지 렌더링',"message":"성공적으로 조회하였습니다."}
     elif _mode =='history':
-        return { "result": "success", "value": '포인트 이력 내역을 확인할 수 있는 페이지 렌더링'}
+        return { "result": "success", "value": '포인트 이력 내역을 확인할 수 있는 페이지 렌더링',"message":"성공적으로 조회하였습니다."}
     elif _mode =='standard':
-        return { "result": "success", "value": '기준과 각각의 점수를 확인할 수 있는 페이지 렌더링'}
+        return { "result": "success", "value": '기준과 각각의 점수를 확인할 수 있는 페이지 렌더링',"message":"성공적으로 조회하였습니다."}
 
 #===============================================================================================기록탭
 #검색어를 통해 검색하거나 항목별로 내가 지금까지 어떤 활동들을 하였는지 모아볼 수 있음
 @app.route('/actSearch/<_keyword>',methods=['GET'])
 def actShow(_keyword):
-    return { "result": "success", "value": "어떤 키워드도 입력하지 않았다면 기본적으로 내가 지금까지 했던 모든 활동들을 보여줌"}
+    return { "result": "success", "value": "어떤 키워드도 입력하지 않았다면 기본적으로 내가 지금까지 했던 모든 활동들을 보여줌","message":"성공적으로 조회하였습니다."}
 
 #==============================================================================================설정탭
 #프로필 사진 바꾸기
 @app.route('/setting/profile',methods=['POST'])
 def changePic():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 
 #비밀번호 바꾸기
 @app.route('/setting/pwd',methods=['POST'])
 def changePwd():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
     # if _mode =='confirm':
     #     data = request.json
     #     return { "result": "success", "value": data}
@@ -228,33 +227,33 @@ def changePwd():
 @app.route('/setting/nickname',methods=['POST'])
 def changeNickname():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 
 #건의 불편신고
 @app.route('/setting/suggest',methods=['POST'])
 def suggest():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 
 #탈퇴하기 -> 데이터베이스에 저장 필요
 @app.route('/setting/withdraw',methods=['POST'])
 def withdraw():
     data = request.json
-    return { "result": "success", "value": data}
+    return { "result": "success", "value": data,"message":"성공적으로 데이터베이스에 저장하였습니다."}
 
 #앱 정보 읽기 기능
 #앱 정보에는 총 4가지가 있는데 각각을 숫자로 구분해서 읽을 수 있도록 해주기 
 @app.route('/setting/info/<_no>',methods=['GET'])
 def readInfo(_no):
-    return { "result": "success", "value": "숫자에 따라서 특정 앱의 정보를 읽을 수 있음"}
+    return { "result": "success", "value": "숫자에 따라서 특정 앱의 정보를 읽을 수 있음","message":"성공적으로 조회하였습니다."}
 
 #푸시 설정
 @app.route('/setting/push/<_mode>',methods=['GET'])
 def push(_mode):
     result = {"choice":_mode}
-    return { "result": "success", "value": result}
+    return { "result": "success", "value": result,"message":"성공적으로 조회하였습니다."}
 
 #===============================================================================================Connection 닫기
 #===============================================================================================서버 실행 부분 
