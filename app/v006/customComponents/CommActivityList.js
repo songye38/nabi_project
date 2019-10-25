5eab8f30f464703fbe892c6069f3236757ed422a
songye38 <ddev0308@gmail.com>
2019-10-25 23:02:56 +0900

    검색화면에서 비대위 내용을 터치해도 비대위 메인화면으로 이동하도록 코드 변경

---
+++ app/v006/customComponents/CommActivityList.js
@@ -0,0 +1,200 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    ScrollView,
+    StyleSheet,
+    TouchableHighlight,
+} from 'react-native';
+import { Icon } from 'react-native-elements';
+import { StackActions, NavigationActions } from 'react-navigation';
+import ScrollableTabView, { DefaultTabBar, ScrollableTabBar } from 'react-native-scrollable-tab-view-forked'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import CommOffAction from './CommOffAction';
+import CommOnAction from './CommOnAction';
+
+
+export default class CommActivityList extends Component{
+
+    static defaultProps = {
+        commName: 'null',
+        content: 'null',
+        count: 0,
+        date: 'null',
+      }
+
+      //<Text style={styles.titleText} numberOfLines={1}>{this.props.commName}</Text>
+    constructor(props){
+        super(props);
+      }
+
+    render(){
+        const { navigate } = this.props.navigation;
+        return (
+            <ScrollView>
+                <View style={styles.listMainContent}>
+                    <View style = {styles.contentSection}>
+                        <View style = {styles.categorySection}>
+                            <Icon name='users' type='feather' size = {28} color = 'black'/>
+                        </View>
+                        <View style = {styles.textSection}>
+                            <View style = {styles.subTextSection}>
+                                <Text style = {styles.subText}>기자회견</Text>
+                                <Text style = {styles.subText}>2019.9.12 목요일</Text>
+                                <Text style = {styles.subText}>오후 2시</Text>
+                                <Text style = {styles.subText}>광화문</Text>
+                            </View>
+                            <TouchableHighlight style = {styles.mainTextSection} onPress={() => navigate('CommOffAction')} underlayColor='white'>
+                                <Text style = {{fontSize : wp('4')}}>비대위 설립을 알리는 기자회견이 있습니다.</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>  
+                </View>  
+                <View style={styles.listMainContent}>
+                    <View style = {styles.contentSection}>
+                        <View style = {styles.categorySection}>
+                            <Icon name='users' type='feather' size = {28} color = 'black'/>
+                        </View>
+                        <View style = {styles.textSection}>
+                            <View style = {styles.subTextSection}>
+                                <Text style = {styles.subText}>온라인행동</Text>
+                                <Text style = {styles.subText}>2019.9.12 목요일</Text>
+                                <Text style = {styles.subText}>오후 2시</Text>
+                                <Text style = {styles.subText}>광화문</Text>
+                            </View>
+                            <View style = {styles.mainTextSection} onPress={() => navigate('CommOnAction')} underlayColor='white'>
+                                <Text style = {{fontSize : wp('4')}}>비대위 설립을 알리는 기자회견이 있습니다.</Text>
+                            </View>
+                        </View>
+                    </View>  
+                </View>  
+                <View style={styles.listMainContent}>
+                    <View style = {styles.contentSection}>
+                        <View style = {styles.categorySection}>
+                            <Icon name='users' type='feather' size = {28} color = 'black'/>
+                        </View>
+                        <View style = {styles.textSection}>
+                            <View style = {styles.subTextSection}>
+                                <Text style = {styles.subText}>기자회견</Text>
+                                <Text style = {styles.subText}>2019.9.12 목요일</Text>
+                                <Text style = {styles.subText}>오후 2시</Text>
+                                <Text style = {styles.subText}>광화문</Text>
+                            </View>
+                            <View style = {styles.mainTextSection}>
+                                <Text style = {{fontSize : wp('4')}}>비대위 설립을 알리는 기자회견이 있습니다.</Text>
+                            </View>
+                        </View>
+                    </View>  
+                </View> 
+                <View style={styles.listMainContent}>
+                    <View style = {styles.contentSection}>
+                        <View style = {styles.categorySection}>
+                            <Icon name='message-circle' type='feather' size = {27}/>
+                        </View>
+                        <View style = {styles.textSection}>
+                            <View style = {styles.subTextSection}>
+                                <Text style = {styles.subText}>기자회견</Text>
+                                <Text style = {styles.subText}>2019.9.12 목요일</Text>
+                                <Text style = {styles.subText}>오후 2시</Text>
+                                <Text style = {styles.subText}>광화문</Text>
+                            </View>
+                            <TouchableHighlight style = {styles.mainTextSection} onPress={() => navigate('CommOnAction')} underlayColor='white'>
+                                <Text style = {{fontSize : wp('4')}}>비대위 설립을 알리는 기자회견이 있습니다.</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>  
+                </View> 
+                <View style={styles.listMainContent}>
+                    <View style = {styles.contentSection}>
+                        <View style = {styles.categorySection}>
+                            <Icon name='users' type='feather' size = {28} color = 'black'/>
+                        </View>
+                        <View style = {styles.textSection}>
+                            <View style = {styles.subTextSection}>
+                                <Text style = {styles.subText}>기자회견</Text>
+                                <Text style = {styles.subText}>2019.9.12 목요일</Text>
+                                <Text style = {styles.subText}>오후 2시</Text>
+                                <Text style = {styles.subText}>광화문</Text>
+                            </View>
+                            <TouchableHighlight style = {styles.mainTextSection}>
+                                <Text style = {{fontSize : wp('4')}}>비대위 설립을 알리는 기자회견이 있습니다.</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>  
+                </View>   
+                <View style={styles.listMainContent}>
+                    <View style = {styles.contentSection}>
+                        <View style = {styles.categorySection}>
+                            <Icon name='users' type='feather' size = {28} color = 'black'/>
+                        </View>
+                        <View style = {styles.textSection}>
+                            <View style = {styles.subTextSection}>
+                                <Text style = {styles.subText}>기자회견</Text>
+                                <Text style = {styles.subText}>2019.9.12 목요일</Text>
+                                <Text style = {styles.subText}>오후 2시</Text>
+                                <Text style = {styles.subText}>광화문</Text>
+                            </View>
+                            <TouchableHighlight style = {styles.mainTextSection}>
+                                <Text style = {{fontSize : wp('4')}}>비대위 설립을 알리는 기자회견이 있습니다.</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>  
+                </View>  
+                <View style={styles.listMainContent}>
+                    <View style = {styles.contentSection}>
+                        <View style = {styles.categorySection}>
+                            <Icon name='users' type='feather' size = {28} color = 'black'/>
+                        </View>
+                        <View style = {styles.textSection}>
+                            <View style = {styles.subTextSection}>
+                                <Text style = {styles.subText}>기자회견</Text>
+                                <Text style = {styles.subText}>2019.9.12 목요일</Text>
+                                <Text style = {styles.subText}>오후 2시</Text>
+                                <Text style = {styles.subText}>광화문</Text>
+                            </View>
+                            <TouchableHighlight style = {styles.mainTextSection}>
+                                <Text style = {{fontSize : wp('4')}}>비대위 설립을 알리는 기자회견이 있습니다.</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>  
+                </View>   
+            </ScrollView>                           
+        );
+    }
+}
+
+const styles = StyleSheet.create({ 
+    listMainContent : {
+        width : '95%',
+        flex : 1,
+        backgroundColor : 'white',
+        flexDirection : 'row',
+        alignItems : 'center',
+        paddingTop : wp(5),
+        paddingBottom : wp(5),
+        borderBottomWidth : wp('0.2'),
+        borderBottomColor : '#eee',
+    },
+    contentSection : {
+        width : wp('100%'),
+        height : wp('10'),
+        flexDirection : 'row',
+    },
+    categorySection : {
+        width : wp('20%'),
+        justifyContent : 'center',
+        alignItems : 'center',
+    },
+    textSection : {
+        flex : 1,
+    }, 
+    subTextSection : {
+        flexDirection : 'row',
+        marginBottom : wp('2'),
+    },
+    subText : {
+        fontSize : wp('3'),
+        paddingRight : wp('1.5'), 
+        fontWeight : 'bold'
+    }
+})
---
+++ app/v006/screens/index.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import {
+    Text
+} from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createMaterialTopTabNavigator, createBottomTabNavigator } from 'react-navigation-tabs' 
+import { Icon } from 'react-native-elements';
+
+
+import LoginScreen from './LoginScreen';
+import HomeScreen from './HomeScreen';
+import SearchScreen from './SearchScreen';
+import ArchiveScreen from './ArchiveScreen';
+import SettingScreen from './SettingScreen';
+import SomethingScreen from './somethingScreen';
+import RegisterScreen from './RegisterScreen';
+import PointScreen from './PointScreen';
+import MyRankScreen from './MyRankScreen';
+import PointHistoryScreen from './PointHistoryScreen';
+import PointStandardScreen from './PointStandardScreen';
+import CommMainScreen from './CommMainScreen';
+import CustomList from '../customComponents/CustomList';
+import CommOffAction from '../customComponents/CommOffAction';
+import CommOnAction from '../customComponents/CommOnAction';
+
+
+const LoginStack = createStackNavigator(
+     {
+        LoginScreen,
+        RegisterScreen
+     },
+     {
+        initialRouteName: 'LoginScreen',
+     }
+
+)
+const HomeStack = createStackNavigator(
+    {
+        HomeScreen,
+        PointScreen,
+        CommMainScreen
+    },
+    {
+        initialRouteName: 'HomeScreen',
+     },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            title: 'Home',
+        }),
+    }
+);
+const SearchStack = createStackNavigator(
+    {
+        SearchScreen,
+        CommMainScreen : CommMainScreen,
+        CommOffAction : CommOffAction,
+        CommOnAction : CommOnAction,
+        CustomList : CustomList,
+    },
+    {
+    defaultNavigationOptions: ({navigation}) => ({
+            title : 'Search',
+    }),
+    }
+);
+
+SearchStack.navigationOptions = ({ navigation }) => {
+  let tabBarVisible;
+  if (navigation.state.routes.length > 1) {
+    navigation.state.routes.map(route => {
+      if (route.routeName === "CommMainScreen" || route.routeName === "CommOffAction" || route.routeName === "CommOnAction") {
+        tabBarVisible = false;
+      } else {
+        tabBarVisible = true;
+      }
+    });
+  }
+  return {
+    tabBarVisible
+  };
+};
+const ArchiveStack = createStackNavigator(
+    {
+        ArchiveScreen,
+        PointScreen,
+        CommMainScreen
+    },
+    {
+        initialRouteName: 'ArchiveScreen',
+     },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            title: 'Archive',
+        }),
+    }
+);
+const SettingStack = createStackNavigator(
+    {
+        SettingScreen,
+        SomethingScreen
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            title: 'Setting',
+        }),
+        initialRouteName: 'SettingScreen',
+    }
+);
+
+
+const TabNavigator = createMaterialTopTabNavigator(
+    {
+        Home: HomeStack,
+        Search: SearchStack,
+        Archive: ArchiveStack,
+        Setting: SettingStack,
+    }, 
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            tabBarIcon: ({focused, horizontal, tintColor}) => {
+                const {routeName} = navigation.state;
+                if(routeName === 'Home'){
+                    return <Icon name='home' type='antdesign' color = {tintColor} size = {20} />
+                } else if(routeName === 'Setting'){
+                    return <Icon name='ellipsis1' type='antdesign' color = {tintColor} size = {20}/>
+                } else if(routeName === 'Search'){
+                    return <Icon name='search1' type='antdesign' color = {tintColor} size = {20}/>
+                } else if(routeName === 'Archive'){
+                    return <Icon name='folder1' type='antdesign' color = {tintColor}size = {20} />
+                } 
+                else if(routeName === 'Point'){
+                    return <Icon name='folder1' type='antdesign' color = {tintColor}size = {20} />
+                } 
+            }
+    }),
+
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    lazy: false,
+    tabBarOptions: {
+    showIcon: true,
+    activeTintColor: "#1abc9c",
+    inactiveTintColor: "black",
+    style: {          
+      height: 60,
+      borderTopWidth: 0.5,
+      borderTopColor: 'black',                      
+  },      
+    labelStyle: {
+      fontSize: 10,
+      marginBottom: 5
+  },
+  style: {
+    backgroundColor: 'white',
+  },
+  indicatorStyle : {
+    borderBottomColor: '#1abc9c',
+    borderBottomWidth: 5,
+  },
+},
+}
+);
+
+const AppStack = createStackNavigator(
+    {
+        LoginScreen: LoginScreen,
+        RegisterScreen : RegisterScreen,
+        TabNavigator: {
+            screen: TabNavigator,
+            navigationOptions: ({navigation}) => ({
+                header: null,
+            }),
+        },
+    }
+);
+
+export default createAppContainer(AppStack);
